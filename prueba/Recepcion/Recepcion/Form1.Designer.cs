15bc31bf6002658585d8c4f3bd6fc141b61f1a76
omega sheron <isradelarosa13@gmail.com>
2018-05-16 01:59:42 -0500

    sigo si entender esta madre

---
+++ prueba/Recepcion/Recepcion/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Recepcion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lbConnections = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(47, 94);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(68, 13);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "Direccion IP ";
+            // 
+            // lbConnections
+            // 
+            this.lbConnections.FormattingEnabled = true;
+            this.lbConnections.Location = new System.Drawing.Point(50, 146);
+            this.lbConnections.Name = "lbConnections";
+            this.lbConnections.Size = new System.Drawing.Size(281, 238);
+            this.lbConnections.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Receptor de imagenes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 428);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbConnections);
+            this.Controls.Add(this.lblStatus);
+            this.Name = "Form1";
+            this.Text = "Receptor ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ListBox lbConnections;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
