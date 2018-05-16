d1a1502f220df87e268b67972492ae936f09d9d3
Israel de la Rosa Ramírez <isradelarosa13@gmail.com>
2018-05-16 13:17:50 -0500

    knkknnk

--- Principal/WindowsFormsApp2/Principal.Designer.cs
+++ Principal/WindowsFormsApp2/Principal.Designer.cs
@@ -303,7 +303,8 @@
             this.Controls.Add(this.Inst);
             this.Controls.Add(this.Nomb);
             this.Name = "Principal";
-            this.Text = "Form1";
+            this.Text = "Compresor de Imagnes de Retina";
+            this.Load += new System.EventHandler(this.Principal_Load);
             ((System.ComponentModel.ISupportInitialize)(this.ImgenBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- Principal/WindowsFormsApp2/Principal.cs
+++ Principal/WindowsFormsApp2/Principal.cs
@@ -6,9 +6,11 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Envio;
 
 namespace WindowsFormsApp2
 {
+   
     public partial class Principal : Form
     {
         public Principal()
@@ -24,16 +26,24 @@ namespace WindowsFormsApp2
         }
 
         private void BtnConv_Click(object sender, EventArgs e)
-        {
-
+        {   
+            BtnCompr.Enabled = true;
         }
 
         private void BtnCompr_Click(object sender, EventArgs e)
         {
-
+            BtnEnviar.Enabled = true;
         }
 
         private void BtnEnviar_Click(object sender, EventArgs e)
+        {
+            Form envio= new Envio.EnvioForm();
+            envio.Show();
+
+
+        }
+
+        private void Principal_Load(object sender, EventArgs e)
         {
 
         }
